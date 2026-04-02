2c1679073b589c6669a1b862752f1144e0503c50
magu1436 <hfzoro77@gmail.com>
2026-04-02 17:57:03 +0900

    refactor: rename kernel directory to kernels fix: correct y-index calculation and scalar type in scalar_multiple kernel

---
+++ kernels/scalar_multiple.cu
@@ -0,0 +1,10 @@
+extern "C" __global__
+void scalar_multiple(const float scalar, const float* array, float* result, int n) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int idx = y * n + x;
+    if (x < n && y < n) {
+        result[idx] = scalar * array[idx];
+    }
+}
