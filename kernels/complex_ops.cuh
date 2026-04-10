a73f3fd7a33a376ee18a56b42b02d66ad22d1043
magu1436 <hfzoro77@gmail.com>
2026-04-10 14:34:18 +0900

    perf: add __forceinline__ to complex helper functions

--- kernels/complex_ops.cuh
+++ kernels/complex_ops.cuh
@@ -1,28 +1,28 @@
 #pragma once
 # include "complex.cuh"
 
-__device__ inline Complex64 cadd(Complex64 a, Complex64 b) {
+__device__ __forceinline__ Complex64 cadd(Complex64 a, Complex64 b) {
     Complex64 z;
     z.re = a.re + b.re;
     z.im = a.im + b.im;
     return z;
 }
 
-__device__ inline Complex64 csub(Complex64 a, Complex64 b) {
+__device__ __forceinline__ Complex64 csub(Complex64 a, Complex64 b) {
     Complex64 z;
     z.re = a.re - b.re;
     z.im = a.im - b.im;
     return z;
 }
 
-__device__ inline Complex64 cmul(Complex64 a, Complex64 b) {
+__device__ __forceinline__ Complex64 cmul(Complex64 a, Complex64 b) {
     Complex64 z;
     z.re = a.re * b.re - a.im * b.im;
     z.im = a.re * b.im + a.im * b.re;
     return z;
 }
 
-__device__ inline Complex64 rscale(double s, Complex64 z) {
+__device__ __forceinline__ Complex64 rscale(double s, Complex64 z) {
     Complex64 result;
     result.re = z.re * s;
     result.im = z.im * s;
