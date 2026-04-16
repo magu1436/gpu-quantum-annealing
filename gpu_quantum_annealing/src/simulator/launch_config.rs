884f90ca4f91c2a2e7ce15e780770e02692fad7a
magu1436 <hfzoro77@gmail.com>
2026-04-16 18:51:34 +0900

    refactor: removed unused config

--- gpu_quantum_annealing/src/simulator/launch_config.rs
+++ gpu_quantum_annealing/src/simulator/launch_config.rs
@@ -2,7 +2,6 @@ use cudarc::driver::LaunchConfig;
 
 pub enum KernelLayout {
     Vector2D,
-    Matrix2D,
     Warp,
 }
 
@@ -11,13 +10,11 @@ pub fn create_launch_config(n: usize, threads_x: u32, layout: KernelLayout) -> L
     
     let block_n = match layout {
         KernelLayout::Vector2D => (n as u32 + threads_x - 1) / threads_x,
-        KernelLayout::Matrix2D => (n as u32 + threads_x - 1) / threads_x,
         KernelLayout::Warp => (n as u32 + warps_per_block - 1) / warps_per_block,
     };
 
     let (block_dim, grid_dim) = match layout {
         KernelLayout::Vector2D => ((threads_x, 1, 1), (block_n, 1, 1)),
-        KernelLayout::Matrix2D => ((threads_x, threads_x, 1), (block_n, block_n, 1)),
         KernelLayout::Warp => ((threads_x, 1, 1), (block_n, 1, 1)),
     };
 
