39da33fc9dc3c1e745f62deda48b57f4f90727f5
magu1436 <hfzoro77@gmail.com>
2026-06-16 16:46:59 +0900

    refactor: change type of args that Worker' constructor needs

--- gpu_quantum_annealing/src/util/worker.rs
+++ gpu_quantum_annealing/src/util/worker.rs
@@ -1,10 +1,9 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{path::Path, sync::Arc};
 
 use cudarc::{driver::{CudaContext, CudaModule, CudaStream}, nvrtc::{CompileOptions, compile_ptx_with_opts}};
 
 use crate::SimResult;
 
-#[repr(C)]
 #[derive(Debug, Clone)]
 pub struct Worker {
     pub ctx: Arc<CudaContext>,
@@ -14,22 +13,22 @@ pub struct Worker {
 
 impl Worker {
     pub fn new(
-        kernel_dir: &str,
-        kernel_file: &str,
+        kernel_dir: impl AsRef<Path>,
+        kernel_file: impl AsRef<Path>,
     ) -> SimResult<Self> {
 
-        let kernel_dir_path = PathBuf::from(&kernel_dir);
+        let kernel_dir_path = kernel_dir.as_ref();
         let kernel_file_path = kernel_dir_path.join(kernel_file);
 
         let ctx = CudaContext::new(0)?;
         let stream = ctx.new_stream()?;
-        let source = std::fs::read_to_string(kernel_file_path).unwrap();
+        let source = std::fs::read_to_string(&kernel_file_path).unwrap();
         let module = ctx.load_module(
             compile_ptx_with_opts(
                 &source,
                 CompileOptions {
                     options: vec![
-                        format!("--include-path={}", kernel_dir_path.to_str().unwrap()),
+                        format!("--include-path={}", kernel_dir_path.display()),
                         "--gpu-architecture=compute_86".to_string(),
                     ],
                     ..Default::default()
